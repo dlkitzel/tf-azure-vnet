7d6129c1186a562503595151dda72b91b7bb0aa6
Derek Kitzelman <derek.kitzelman@gmail.com>
2020-11-09 15:57:31 -0600

    Add files via upload
    initial upload

---
+++ variables.tf
@@ -0,0 +1,70 @@
+variable "vnet_name" {
+  description = "Name of the vnet to create"
+  type        = string
+  default     = "dktest-vnet01"
+}
+
+variable "subscriptionId" {}
+variable "clientId" {}
+variable "clientSecret" {}
+variable "tenantId" {}
+
+variable "resource_group_name" {
+  description = "Name of the resource group to be imported."
+  type        = string
+  default     = "dkitxdemo"
+}
+
+variable "address_space" {
+  type        = list(string)
+  description = "The address space that is used by the virtual network."
+  default     = ["10.0.0.0/16"]
+}
+
+# If no values specified, this defaults to Azure DNS 
+variable "dns_servers" {
+  description = "The DNS servers to be used with vNet."
+  type        = list(string)
+  default     = []
+}
+
+variable "subnet_prefixes" {
+  description = "The address prefix to use for the subnet."
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "subnet_names" {
+  description = "A list of public subnets inside the vNet."
+  type        = list(string)
+  default     = ["subnet1", "subnet2", "subnet3"]
+}
+
+variable "subnet_service_endpoints" {
+  description = "A map of subnet name to service endpoints to add to the subnet."
+  type        = map(any)
+  default     = {}
+}
+
+variable "nsg_ids" {
+  description = "A map of subnet name to Network Security Group IDs"
+  type        = map(string)
+
+  default = {
+  }
+}
+
+variable "route_tables_ids" {
+  description = "A map of subnet name to Route table ids"
+  type        = map(string)
+  default     = {}
+}
+
+variable "tags" {
+  description = "The tags to associate with your network and subnets."
+  type        = map(string)
+
+  default = {
+    ENV = "test"
+  }
+}
