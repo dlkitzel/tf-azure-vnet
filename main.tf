18ffb46343e332bbf1e49ec9a1edcbb8199e2bfd
Derek Kitzelman <derek.kitzelman@gmail.com>
2020-11-10 10:38:28 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,24 +19,3 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_prefixes[count.index]]
 }
-
-data "azurerm_subnet" "import" {
-  for_each             = var.nsg_ids
-  name                 = each.key
-  resource_group_name  = data.azurerm_resource_group.vnet.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-
-  depends_on = [azurerm_subnet.subnet]
-}
-
-resource "azurerm_subnet_network_security_group_association" "vnet" {
-  for_each                  = var.nsg_ids
-  subnet_id                 = data.azurerm_subnet.import[each.key].id
-  network_security_group_id = each.value
-}
-
-resource "azurerm_subnet_route_table_association" "vnet" {
-  for_each       = var.route_tables_ids
-  route_table_id = each.value
-  subnet_id      = data.azurerm_subnet.import[each.key].id
-}
