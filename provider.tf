d46c08bbf72c16824a8635316388e87381ceb8a8
Derek Kitzelman <derek.kitzelman@gmail.com>
2020-11-10 00:05:51 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   
-  subscriptionId = var.subscription_id
-  clientId       = var.client_id
-  clientSecret   = var.client_secret
-  tenantId       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 
   features {}
 }
